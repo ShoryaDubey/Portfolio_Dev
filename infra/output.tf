0dc123ec71725e8e4efe1b128c9edba327e3dfb9
Shorya Dubey <shorya.dubey@v2solutions.com>
2025-07-22 12:36:33 +0530

    added output

--- infra/output.tf
+++ infra/output.tf
@@ -3,3 +3,9 @@ output "alb_dns_name" {
   value       = aws_lb.app_alb.dns_name
   description = "The DNS name of the Application Load Balancer."
 }
+
+output "asg_name" {
+  value       = aws_autoscaling_group.app_asg.name
+  description = "The name of the Auto Scaling Group."
+  
+}
